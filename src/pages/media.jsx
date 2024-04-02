a81ff5e9cdc8c7aa237950c9b1437f6db278daaf
JAlan <GeeWhyAlan@outlook.com>
2024-04-02 07:51:04 -0500

    start fetching userdata

--- src/pages/media.jsx
+++ src/pages/media.jsx
@@ -2,27 +2,96 @@ import { React, useEffect, useState} from "react";
 import UserPanel from "../components/media/UserPanel";
 import UploadDialogue from "../components/media/UploadDialogue";
 import Posts from "../components/media/Posts/posts";
-
+import { useNavigate } from "react-router-dom";
+import { fetchWithTimeout } from "../Utils";
 
 import "./media.css"
 
+
+const DEMOGROUP = 1
+
 const Media = () => {
+    const navigate = useNavigate()
 
     const [posts, setPosts] =  useState([])
 
+
     useEffect(() => {
+        if (localStorage.getItem("currentUserID") === null) {
+            navigate("/login")
+            return
+        }
+    }, [])    
+
+    useEffect(() => {
+        var storedMessages
+
         try {
-            const storedMessages = JSON.parse(localStorage.getItem("posts"))
-            if (!(storedMessages === undefined || storedMessages.length == 0)) setPosts(storedMessages)
+            storedMessages = JSON.parse(localStorage.getItem(`Group${DEMOGROUP}`))
+            if (!(storedMessages === undefined || storedMessages.length == 0)) {
+                // check to make sure is up to date
+                
+                fetchWithTimeout(
+                    `${process.env.EXPO_PUBLIC_API_URL}/groups/fetchLatestPostID/${DEMOGROUP}`
+                ).then(
+                    (res) => {
+                        if (!res.ok) {
+                            // TODO: add bad case where server fails
+                            // probably where fails to fetch due to incorrect session cookie
+                            console.log(res.status)
+                            throw "ServerError"
+                        }
+                        return res.json()
+                    }
+                ).then(
+                    (data) => {
+                        
+                    }
+                ).catch(
+                    (e) => {
+                        console.log(e)
+                    }
+                )
+
+                
+                
+                setPosts(storedMessages)
+                return
+            }
         } catch(e) {
         }
+
+
+        fetchWithTimeout(
+            `${process.env.EXPO_PUBLIC_API_URL}/groups/fetchPostRange/${DEMOGROUP}?`
+            + new URLSearchParams({
+                
+            })
+        ).then(
+            (res) => {
+                if (!res.ok) {
+                  // TODO: add bad case where server fails
+                  console.log(res.status)
+                  throw "ServerError"
+                }
+                return res.json()
+            }
+        ).then(
+            (data) => {
+
+            }
+        ).catch(
+            (e) => {
+                console.log(e)
+            }
+        )
         
 
     }, [])
 
     
     useEffect(() => {
-        localStorage.setItem("posts", JSON.stringify(posts))
+        localStorage.setItem(`Group${DEMOGROUP}`, JSON.stringify(posts))
     }, [posts])
 
     function addMessage(message, image) {
