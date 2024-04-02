df7992214c02ff4540988aa928756d1f3f23d057
JAlan <GeeWhyAlan@outlook.com>
2024-04-01 19:07:11 -0500

    add login

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Media from "./pages/media"
 import Home from "./pages/home"
 import Donate from "./pages/donate";
+import Login from "./pages/login";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Route exact path="/" element={<Home/>}/>
             <Route path="/media" element={<Media/>}/>
             <Route path="/donate" element={<Donate/>}/>
+            <Route path="/login" element={<Login/>}/>
           </Routes>
 
   );
---
+++ src/pages/login.jsx
@@ -0,0 +1,84 @@
+import './login.css';
+import { useEffect, useRef, useState } from "react"
+import Navbar from '../components/home/Navbar';
+import { Link } from "react-router-dom"
+import { fetchWithTimeout } from '../Utils';
+import { useNavigate } from 'react-router-dom';
+
+const Login = () => {
+    const navigate = useNavigate()
+
+    const correctLogin = () => {
+        navigate("/media")
+    }
+
+    const incorrectLogin = () => {
+        document.getElementById('input-container').elements[1].value = ""
+        
+        setTimeout(() => {
+            alert("Username or Password Incorrect")
+        }, 50)
+    }
+    
+    const attemptLogin = () => {
+        fetchWithTimeout(`${process.env.REACT_APP_BACKEND_API}/auth/login`, {
+            method: "POST",
+            headers: {'Accept': 'application/json', 'Content-Type': 'application/json'}, 
+            body: JSON.stringify({
+                'username': document.getElementById('input-container').elements[0].value, 
+                'password': document.getElementById('input-container').elements[1].value
+            }),
+            timeout: 3000
+          }).then(
+            (res) => {
+              (res.ok) ? correctLogin(): incorrectLogin()
+              // console.log(res.status)
+                
+              // console.log(res.headers)
+              // return res.json()
+            }
+          ).catch(
+            // failed to login due to timeout
+            () => {
+              alert("Could not connect to our servers. Check your internet connection")
+            }
+        )
+    }
+
+    const attemptRegister = () => {
+        
+    }
+
+
+    return (
+            <div className="Login" >
+                <Link to="/" id="link">
+                    <img className="menu" src="/assets/menu.png"></img>
+                </Link>
+                <div id="login-container">
+                    <h1 id='login-title'>Login</h1>
+
+                    <form id='input-container'>
+                        <input
+                            type='text'
+                            placeholder='username'
+                            id='username-field'
+                        />
+                        <input
+                            type='password'
+                            placeholder='password'
+                            id='password-field'
+                        />
+                    </form>
+
+                    <div id='submit-container'>
+                        <button id='register-button'>Register</button>
+                        <button onClick={attemptLogin} id='login-button'>Login</button>
+                    </div>
+                </div>
+                
+            </div>
+    )
+}
+
+export default Login
